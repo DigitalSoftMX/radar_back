96e13086332e58ea9121e959772f21d88042576e
ulises suarez <sherlocklewisus@gmail.com>
2022-04-29 15:58:29 -0500

    make

--- controller/RouterController.js
+++ controller/RouterController.js
@@ -11,11 +11,10 @@ exports.PlacesYPrices = async function(req, res) {
     try {
         const urlPlaces = 'https://publicacionexterna.azurewebsites.net/publicaciones/places'
         const urlPrices = 'https://publicacionexterna.azurewebsites.net/publicaciones/prices'
-        let Place, Price = {}
+        let Place, Price, Location = {}
         let placeYPrice = {
             
         }
-        let location = []
         await xmlToJson(urlPrices).then((data)=>{
             Price = data['places']['place']
         })
@@ -23,7 +22,7 @@ exports.PlacesYPrices = async function(req, res) {
             Place = data['places']['place']
         })
         for (let i = 0; i < 1; i++) {
-            const urlLocation = `https://maps.googleapis.com/maps/api/place/nearbysearch/xml?location=${Place[i].location[0].y[0]},${Place[i].location[0].x[0]}&radius=20&type=gas_station&key=AIzaSyDAYDRUB8-MNmO6JAy0aHaNaOKmE5VZHpI`
+            const urlLocation = `https://maps.googleapis.com/maps/api/place/nearbysearch/xml?location=${Place[i].location[0].y[0]},${Place[i].location[0].x[0]}&radius=10&type=gas_station&key=AIzaSyDAYDRUB8-MNmO6JAy0aHaNaOKmE5VZHpI`
             await xmlToJson(urlLocation).then((data)=>{
                 location.push(data)
                 //console.dir(data)
