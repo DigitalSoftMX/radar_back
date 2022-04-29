0fa1c3663de3c4debd0a593063657c454f50a4c9
ulises suarez <sherlocklewisus@gmail.com>
2022-04-29 16:46:14 -0500

    make

--- controller/RouterController.js
+++ controller/RouterController.js
@@ -25,7 +25,7 @@ exports.PlacesYPrices = async function(req, res) {
             if (i == 4) {
                 console.log(Place[4])
                 console.log("y:",Place[i].location[0].y[0],"x:",Place[i].location[0].x[0]);
-                const urlLocation = `https://maps.googleapis.com/maps/api/place/nearbysearch/xml?location=${Place[i].location[0].y[0]},${Place[i].location[0].x[0]}&radius=40&type=gas_station&key=AIzaSyDAYDRUB8-MNmO6JAy0aHaNaOKmE5VZHpI`
+                const urlLocation = `https://maps.googleapis.com/maps/api/place/nearbysearch/xml?location=${Place[i].location[0].y[0]},${Place[i].location[0].x[0]}&radius=40&type=gas_station&keyword=gas_station&key=AIzaSyDAYDRUB8-MNmO6JAy0aHaNaOKmE5VZHpI`
                 await xmlToJson(urlLocation).then((data)=>{
                     Location = data
                     //console.dir(data)
