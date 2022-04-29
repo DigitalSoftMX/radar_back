9ace32508fcd5316b9c05e9b50314b92d986fb04
ulises suarez <sherlocklewisus@gmail.com>
2022-04-29 13:50:41 -0500

    make

--- function/xmlToJson.js
+++ function/xmlToJson.js
@@ -29,7 +29,7 @@ var loadBody2 = function (res) {
     res.on("data", function (chunk) {
         let json = JSON.stringify(chunk)
         var buf = Buffer.from(JSON.parse(json).data)
-        var temp = buf.toString()
+        var temp = buf.toString('utf8')
         var temp2 = JSON.parse(temp) 
        //data =  JSON.parse(chunk.toString());
       //data += chunk.toString()
