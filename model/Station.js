8868f0726a1529a463028b8f9353725e57d2fbff
Fernando-Lopez <development@digitalquo.com>
2022-06-01 16:59:46 -0500

    Creating stations models

---
+++ model/Station.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+let Schema = mongoose.Schema
+const stationSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: [true, 'El nombre de la estación es requerido']
+    },
+    CRE: {
+        type: String,
+        trim: true,
+        unique: true,
+        match: [ /[A-ZÑ&]{2}[/][0-9]{4,}[/][A-ZÑ&]{3}[/][A-ZÑ&]{2}[/][0-9]{4}$/i, 'Ingrese un CRE válido']
+    },
+    competitors: {
+        type: Schema.Types.ObjectId,
+        trim: true,
+        ref: 'competitors'
+    }
+})
---
+++ model/StationCompetitor.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+let schema = mongoose.Schema
+const competitorsSchema = new schema({
+    peso: {
+        type: String
+    },
+    distance: {
+        type: Number
+    },
+    CRE: {
+        type: String, 
+        trim: true, 
+        index: {sparse: true, unique: true}, 
+        match: [ /[A-ZÑ&]{2}[/][0-9]{4,}[/][A-ZÑ&]{3}[/][A-ZÑ&]{2}[/][0-9]{4}$/i, 'Ingrese un CRE válido']
+    },
+    companyName: {
+        type: String,
+        required: [true, 'El nombre de la compañia es requerido']
+    },
+    adress: {
+        type: String,
+        required: [true, 'La dirección es requerida']
+    },
+    flag: {
+        type: String,
+        required: [true, 'La bandera es requerida']
+    }
+}, { timestamps: {}})
+
+module.exports = mongoose.model('competitors', competitorsSchema)
