c950903834c8df5c982f672b489d80931f2f058e
Fernando-Lopez <development@digitalquo.com>
2022-06-02 16:07:45 -0500

    place modification

--- function/place.js
+++ function/place.js
@@ -4,9 +4,10 @@ let placeYPrice = {
 async function Place() {
     const urlPlaces = 'http://publicacionexterna.azurewebsites.net/publicaciones/places'
     let places = {}
-    intervalFunc =  await xmlToJson(urlPrices).then((data)=>{
+    intervalFunc =  await xmlToJson(urlPlaces).then((data)=>{
         places = data['places']['place']
         //setTimeout(Place, 150);
+        console.log(places);
     })
     return places
 }
