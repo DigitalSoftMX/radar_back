db7dafe42e0f8c08b64db491901f939218611684
Fernando-Lopez <development@digitalquo.com>
2022-06-03 10:22:29 -0500

    Adding User model

---
+++ model/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+let Schema = mongoose.Schema
+const userSchema = new Schema({
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'El correo es requerido']
+    },
+    password: {
+        type: String,
+        trim: true, 
+        required: [true, 'La contraseña es requeridas'],
+        match: [/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i, 'Ingrese un correo valido' ]
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: [true, 'El nombre es requerido']
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        required: [true, 'El apellido es requerido']
+    }
+})
