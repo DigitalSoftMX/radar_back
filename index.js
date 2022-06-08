3938a9c35bf76f3a47af3382d52c9e972077044a
ulises suarez <sherlocklewisus@gmail.com>
2022-06-08 09:55:17 -0500

    final

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ app.use(require('./router/router'))
 //===============================================================================================
 //            DATABASE
 //===============================================================================================
-
-mongoose.connect(`${URI}`, { useNewUrlParser: true, useUnifiedTopology: true })
+console.log(URI);
+mongoose.connect(`${URI}`,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then(msg => console.log("DB online"))
     .catch(error => console.log(error)) 
 
