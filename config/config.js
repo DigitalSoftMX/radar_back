6f33a6e1d8c6a856652acb3eaa696f3084554696
ulises suarez <sherlocklewisus@gmail.com>
2022-06-08 09:58:22 -0500

    final

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
-const PORT = process.env.PORT || 3200
 require('dotenv').config()
-let URI = process.env.URI
+const PORT = process.env.PORT || 3200
+const URI = process.env.URI
+console.log(URI);
 module.exports = {
     PORT,
     URI
