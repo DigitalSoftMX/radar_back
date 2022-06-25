52ca9cf32e04bcdbff6dc08c3e20474902f9f14e
ulises suarez <sherlocklewisus@gmail.com>
2022-06-25 16:03:55 -0500

    init comit

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const PORT = process.env.PORT || 3200
 require('dotenv').config()
-let URI = process.env.URI
+let URI = 'mongodb+srv://Onexpo:x4IIGYxGgVClnYC8@cluster0.9lsvc.mongodb.net/radar?retryWrites=true&w=majority'
 let TAXAPI = process.env.FACTURA_API
 module.exports = {
     PORT,
